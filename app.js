178c9752721d9bd1fd0d16e481ceb00965cea30a
hariprasanthmath <hariprasanthmath@gmail.com>
2022-12-26 20:19:02 +0530

    backend

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
+//this is backend
 app.get("/", (req, res) => res.type('html').send(html));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
